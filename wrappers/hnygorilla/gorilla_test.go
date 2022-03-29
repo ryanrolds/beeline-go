69c2cb9929c061a5ff23b8a8c9f44e1ea8f8d891
Ryan Olds <ryan.olds@oden.io>
2022-03-29 16:39:47 -0700

    Removed test that will get rewritten

--- wrappers/hnygorilla/gorilla_test.go
+++ wrappers/hnygorilla/gorilla_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/gorilla/mux"
 	beeline "github.com/honeycombio/beeline-go"
-	"github.com/honeycombio/beeline-go/wrappers/config"
 	libhoney "github.com/honeycombio/libhoney-go"
 	"github.com/honeycombio/libhoney-go/transmission"
 	"github.com/stretchr/testify/assert"
@@ -53,57 +52,6 @@ func TestGorillaMiddleware(t *testing.T) {
 		name, ok := fields["gorilla.vars.name"]
 		assert.True(t, ok, "gorilla.vars.name field must exist on middleware generated event")
 		assert.Equal(t, "pooh", name, "successfully served request should have name var populated")
-	})
-
-	t.Run("struct handler should not panic", func(t *testing.T) {
-		// build a sample request to generate an event
-		r, _ := http.NewRequest("GET", "/yo", nil)
-		w := httptest.NewRecorder()
-		// handle the request
-		router.ServeHTTP(w, r)
-
-		evs := mo.Events()
-		assert.Equal(t, 2, len(evs))
-		assert.Equal(t, "testHandler", evs[1].Data["name"])
-	})
-}
-
-func TestGorillaMiddlewareWithConfig(t *testing.T) {
-	// set up libhoney to catch events instead of send them
-	mo := &transmission.MockSender{}
-	client, err := libhoney.NewClient(libhoney.ClientConfig{
-		APIKey:       "placeholder",
-		Dataset:      "placeholder",
-		APIHost:      "placeholder",
-		Transmission: mo})
-	assert.Equal(t, nil, err)
-	beeline.Init(beeline.Config{Client: client})
-
-	incomingConfig := config.HTTPIncomingConfig{}
-
-	// build the gorilla mux router with Middleware
-	router := mux.NewRouter()
-	router.Use(MiddlewareWithConfig(incomingConfig))
-	router.HandleFunc("/hello/{name}", func(_ http.ResponseWriter, _ *http.Request) {})
-	router.Handle("/yo", testHandler{})
-
-	t.Run("function handler", func(t *testing.T) {
-		// build a sample request to generate an event
-		r, _ := http.NewRequest("GET", "/hello/pooh", nil)
-		r.Header.Add("Referer", "https://www.honeycomb.io/")
-
-		w := httptest.NewRecorder()
-		// handle the request
-		router.ServeHTTP(w, r)
-
-		// verify the MockOutput caught the well formed event
-		evs := mo.Events()
-		assert.Equal(t, 1, len(evs), "one event is created with one request through the Middleware")
-		fields := evs[0].Data
-		// verify that referer is set in span
-		referer, ok := fields["request.referrer"]
-		assert.True(t, ok, "request.referrer field must exist on middleware generated event")
-		assert.Equal(t, "https://www.honeycomb.io/", referer, "referer var populated")
 	})
 
 	t.Run("struct handler should not panic", func(t *testing.T) {
